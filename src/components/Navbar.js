de8938874bcb9bf5f09718d754cad803f5f2efff
Nate Shipp <nateskate256@gmail.com>
2021-05-04 21:08:44 -0700

    fixing logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar(props) {
   return (
     <header class="main-header">
-      <img
+      <img a href="#https://shipp-portfolio.netlify.app/"
         src="https://pro2-bar-s3-cdn-cf6.myportfolio.com/017b7213-1eb1-4d62-a026-2ddfebce2d73/0baa77fd-c629-4961-8516-2a59d21861d3_rwc_0x0x892x273x4096.png?h=bbfd890e16a05034dfd060ce6b500417"
         alt="header"
       />
