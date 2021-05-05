2c58adf97dea8d6c8066496b0b144093b3ccea06
Nate Shipp <nateskate256@gmail.com>
2021-05-04 21:14:50 -0700

    added link to logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,11 @@ import React from "react";
 function Navbar(props) {
   return (
     <header class="main-header">
-      <img a href="#https://shipp-portfolio.netlify.app/"
+      <a href="https://shipp-portfolio.netlify.app/">
+      <img 
         src="https://pro2-bar-s3-cdn-cf6.myportfolio.com/017b7213-1eb1-4d62-a026-2ddfebce2d73/0baa77fd-c629-4961-8516-2a59d21861d3_rwc_0x0x892x273x4096.png?h=bbfd890e16a05034dfd060ce6b500417"
-        alt="header"
-      />
+        alt="header"/>
+      </a>
       <nav>
         <a href="#About-Me">About Me</a>
         <a href="#Contact">Contact</a>
